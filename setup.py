7c04f02bd25aab779e131c68e1b3fb8a9296e0ee
Andrew Berger <andbberger@gmail.com>
2022-09-02 18:22:39 -0700

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="PIJM",
+    version="0.1.1",
+    author="Andrew Berger",
+    author_email="a5b@stanford.edu",
+    description="PIJM is just matplotlib. Tools for visualization of dense 3D imagery.",
+    url="https://github.com/ClandininLab/pijm",
+    packages=["pijm"],
+    python_requires=">=3.6",
+    install_requires=[
+        "matplotlib",
+    ],
+)
